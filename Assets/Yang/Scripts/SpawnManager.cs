c182bb82acdebd39fa3dcabf1ce8163fb6b826be
Xun Zhu <zhu.xun1@northeastern.edu>
2023-03-19 00:58:31 -0700

    Add some functionalities to danceWall.

---
+++ Assets/Yang/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] danceWallPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomDanceWall", 2, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnRandomDanceWall()
+    {
+        int danceWallIndex = Random.Range(0, danceWallPrefabs.Length);
+        Vector3 spawnPos = new Vector3((float)1538.5, 641, 100);
+        Instantiate(danceWallPrefabs[danceWallIndex], spawnPos, danceWallPrefabs[danceWallIndex].transform.rotation);
+    }
+}
