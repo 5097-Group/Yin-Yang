0e52c3b404dd99d8058dea77601d315afeaca907
Sophia Wu <114020121@link.cuhk.edu.cn>
2023-03-23 16:20:22 -0700

    upload the position of wall & the way wall show up

--- Assets/Yang/Scripts/SpawnManager.cs
+++ Assets/Yang/Scripts/SpawnManager.cs
@@ -8,19 +8,31 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomDanceWall", 5, 1.5f);
+        DisableWalls();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void EnableWalls()
+    {
+        InvokeRepeating("SpawnRandomDanceWall", 0, 1.5f);
+
+    }
+
+    public void DisableWalls()
+    {
+        CancelInvoke();
     }
 
     void SpawnRandomDanceWall()
     {
         int danceWallIndex = Random.Range(0, danceWallPrefabs.Length);
-        Vector3 spawnPos = new Vector3((float)1538.5, 641, 100);
+        Vector3 spawnPos = new Vector3(-74,-7,30);
+        //Vector3 spawnPos = new Vector3(transform.position);
         Instantiate(danceWallPrefabs[danceWallIndex], spawnPos, danceWallPrefabs[danceWallIndex].transform.rotation);
     }
 }
