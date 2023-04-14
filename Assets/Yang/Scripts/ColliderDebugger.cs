69175ace10fd0f386a943e2605da8264c1ea3179
Rayzzzx <rayzzzx13@gmail.com>
2023-04-14 16:25:32 -0700

    bugs fixed and delete score sys

--- Assets/Yang/Scripts/ColliderDebugger.cs
+++ Assets/Yang/Scripts/ColliderDebugger.cs
@@ -4,6 +4,6 @@ public class ColliderDebugger : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("OnTriggerEnter called for: " + other.gameObject.name);
+        // Debug.Log("OnTriggerEnter called for: " + other.gameObject.name);
     }
 }
